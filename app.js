e6a2496a2a7aa89be5c780e9493003be8e7b7027
Nedzad Alibegovic <git@nedzad.dev>
2020-01-21 17:06:06 +0100

    Remove need for server.js and some housekeeping

--- app.js
+++ app.js
@@ -1,53 +1,20 @@
-require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const app = express();
-
 const cityRoutes = require('./api/routes/cities');
 
+const app = express();
+
 // establish db connection
-mongoose.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: process.env.DB_NAME
-});
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// logging
-app.use(morgan('dev'));
-
-// request headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET');
-        return res.status(200).json({});
-    }
-
-    next();
-});
-
-// api routes
+// middlewares
+app.use(cors());
 app.use('/cities', cityRoutes);
 
-// not found error creation
-app.use((req, res, next) => {
-    const error = new Error('Invalid route');
+// routes
+app.get('/', (req, res) => {
+    res.redirect(302, process.env.REDIRECT);
+})
 
-    error.status = 404;
-    next(error);
-});
-
-// error handling
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
-
-module.exports = app;
+app.listen(process.env.PORT);
